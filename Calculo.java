83769428033eeb0f1d8664b69c94156c9e401c45
Kalebe <kalebefurlan@edu.unifil.br>
2023-03-30 20:19:48 -0300

    calculo de f(x)=x³-9x+3

--- Calculo.java
+++ Calculo.java
@@ -1,1 +1,23 @@
-matheuszinho
+import java.util.Scanner;
+
+import java.util.Scanner;
+
+public class Calculo {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Digite o valor de x: ");
+        double x = sc.nextDouble();
+
+        double resultado = calcularFuncao(x);
+
+        System.out.println("O resultado da função para x = " + x + " é " + resultado);
+
+        sc.close();
+    }
+
+    public static double calcularFuncao(double x) {
+        double resultado = Math.pow(x, 3) - 9 * x + 3;
+        return resultado;
+    }
+}
